7c2584d464222cf0dcc143403154cb504c5447e4
Precious Chikezie <ebubzy1997@gmail.com>
2025-05-29 02:03:27 +0100

    Point to the right route

--- src/pages/Vendor/FundWallet/VFWScreenThreePaymentStatus.jsx
+++ src/pages/Vendor/FundWallet/VFWScreenThreePaymentStatus.jsx
@@ -37,7 +37,7 @@ const VFWScreenThreePaymentStatus = () => {
 
         if (statusParam && txRefParam) {
           const response = await axios.post(
-            `${API_BASE_URL}/payments`,
+            `${API_BASE_URL}/wallet/payment`,
             {
               status: statusParam,
               tx_ref: txRefParam,
